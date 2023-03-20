a926e342cb7aa2031f88927c2c0d75ca770a485c
fatouuuu <fatmaabdullrahim@gmail.com>
2023-03-20 16:53:57 +0300

    completed optional assignment

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,23 @@
 class StudentsController < ApplicationController
+  def show
+    @student = Student.find(params[:id])
+    render json: @student
+  end
 
   def index
-    students = Student.all
-    render json: students
+    @students = Student.all
+    render json: @students
   end
+
+  # Students GET /students with query params returns students whose first or last name matches the query
+  def index
+    if params[:q]
+      @students = Student.where("first_name LIKE :query OR last_name LIKE :query", query: "%#{params[:name]}%")    
+    else
+      @students = Student.all
+    end
+    render json: @students
+  end
+  
 
 end
